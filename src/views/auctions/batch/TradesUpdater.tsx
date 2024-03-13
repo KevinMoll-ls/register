961c3fa7b06f5537ca899487aecd4d6cf65bab97
Luis Camargo <luiska1993@gmail.com>
2024-03-13 11:46:15 -0600

    chores: trades from chain

--- src/views/auctions/batch/TradesUpdater.tsx
+++ src/views/auctions/batch/TradesUpdater.tsx
@@ -3,10 +3,19 @@ import { gql } from 'graphql-request'
 import useDebounce from 'hooks/useDebounce'
 import useQuery from 'hooks/useQuery'
 import useRToken from 'hooks/useRToken'
-import { useAtomValue, useSetAtom } from 'jotai'
+import { atom, useAtomValue, useSetAtom } from 'jotai'
 import { useEffect } from 'react'
-import { blockTimestampAtom } from 'state/atoms'
+import {
+  blockTimestampAtom,
+  chainIdAtom,
+  rTokenAssetsAtom,
+  rTokenContractsAtom,
+} from 'state/atoms'
 import { tradesAtom } from '../atoms'
+import { atomWithLoadable } from 'utils/atoms/utils'
+import { Address } from 'viem'
+import BackingManager from 'abis/BackingManager'
+import { readContracts } from '@wagmi/core'
 
 const tradesQuery = gql`
   query Trades($id: String!, $time: Int!) {
@@ -48,8 +57,46 @@ const tradesQuery = gql`
   }
 `
 
+const openTradesFromChainAtom = atomWithLoadable(async (get) => {
+  const contracts = get(rTokenContractsAtom)
+  const assetRegistry = get(rTokenAssetsAtom)
+  const chain = get(chainIdAtom)
+
+  console.log('contracts', { contracts, assetRegistry })
+
+  if (!contracts?.backingManager || !assetRegistry) {
+    return null
+  }
+
+  try {
+    const calls = Object.keys(assetRegistry).map((asset) => {
+      return {
+        address: contracts.backingManager.address as Address,
+        abi: BackingManager,
+        functionName: 'trades',
+        args: [asset as Address],
+        chainId: chain,
+      }
+    })
+
+    console.log('trying to get trades')
+
+    const result = await readContracts({
+      contracts: calls,
+      allowFailure: false,
+      blockNumber: 19424830n,
+    })
+
+    console.log('result', result)
+  } catch (e) {
+    console.error('Failed to get trades', e)
+    return null
+  }
+})
+
 const TradesUpdater = () => {
   const rToken = useRToken()
+  const openTrades = useAtomValue(openTradesFromChainAtom)
   const blockTimestamp = useDebounce(
     useAtomValue(blockTimestampAtom) || dayjs().unix(),
     60000
@@ -60,6 +107,8 @@ const TradesUpdater = () => {
     id: rToken?.address.toLowerCase(),
     time: blockTimestamp,
   })
+
+  console.log('open trades', openTrades)
 
   useEffect(() => {
     if (data) {
--- src/views/auctions/dutch/components/useDutchTrades.tsx
+++ src/views/auctions/dutch/components/useDutchTrades.tsx
@@ -3,13 +3,25 @@ import useQuery from 'hooks/useQuery'
 import useRToken from 'hooks/useRToken'
 import { atom, useAtom, useAtomValue, useSetAtom } from 'jotai'
 import { useEffect } from 'react'
-import { blockAtom } from 'state/atoms'
+import {
+  blockAtom,
+  chainIdAtom,
+  rTokenAssetsAtom,
+  rTokenContractsAtom,
+} from 'state/atoms'
 import atomWithDebounce from 'utils/atoms/atomWithDebounce'
 import {
   endedDutchTradesAtom,
   ongoingDutchTradesAtom,
   pendingDutchTradesAtom,
 } from '../atoms'
+import { atomWithLoadable } from 'utils/atoms/utils'
+import { Address, readContracts } from 'wagmi'
+import BackingManager from 'abis/BackingManager'
+import { formatUnits, zeroAddress } from 'viem'
+import DutchTrade from 'abis/DutchTrade'
+import { publicClient } from 'state/chain'
+import { TradeKind } from 'views/auctions/atoms'
 
 const ongoingTradesQuery = gql`
   query OngoingDutchTrades($id: String!, $block: Int!) {
@@ -93,12 +105,131 @@ const currentBlockAtom = atom((get) => {
   return debounced || block
 })
 
+const openTradesFromChainAtom = atomWithLoadable(async (get) => {
+  const contracts = get(rTokenContractsAtom)
+  const assetRegistry = get(rTokenAssetsAtom)
+  const chain = get(chainIdAtom)
+  const client = publicClient({ chainId: chain })
+
+  if (!contracts?.backingManager || !assetRegistry) {
+    return null
+  }
+
+  try {
+    const calls = Object.keys(assetRegistry).map((asset) => {
+      return {
+        address: contracts.backingManager.address as Address,
+        abi: BackingManager,
+        functionName: 'trades',
+        args: [asset as Address],
+        chainId: chain,
+      }
+    })
+
+    console.log('trying to get trades')
+
+    const result = await readContracts({
+      contracts: calls,
+      allowFailure: false,
+    })
+
+    const openTradesCalls = result
+      .filter((addr) => addr !== zeroAddress)
+      .map((addr) => [
+        {
+          address: addr as Address,
+          abi: DutchTrade,
+          functionName: 'worstPrice',
+          chainId: chain,
+        },
+        {
+          address: addr as Address,
+          abi: DutchTrade,
+          functionName: 'endTime',
+          chainId: chain,
+        },
+        {
+          address: addr as Address,
+          abi: DutchTrade,
+          functionName: 'endBlock',
+          chainId: chain,
+        },
+        {
+          address: addr as Address,
+          abi: DutchTrade,
+          functionName: 'startBlock',
+          chainId: chain,
+        },
+        {
+          address: addr as Address,
+          abi: DutchTrade,
+          functionName: 'sell',
+          chainId: chain,
+        },
+        {
+          address: addr as Address,
+          abi: DutchTrade,
+          functionName: 'buy',
+          chainId: chain,
+        },
+        {
+          address: addr as Address,
+          abi: DutchTrade,
+          functionName: 'sellAmount',
+          chainId: chain,
+        },
+      ])
+
+    if (!openTradesCalls.length) {
+      return null
+    }
+
+    const trade = await readContracts({
+      contracts: openTradesCalls[0],
+      allowFailure: false,
+    })
+
+    const sellingToken = assetRegistry[trade[4] as Address]
+
+    const startBlock = await client.getBlock({
+      blockNumber: trade[3] as bigint,
+    })
+
+    return {
+      id: openTradesCalls[0][0].address,
+      amount: +formatUnits(
+        trade[6] as bigint,
+        sellingToken?.token?.decimals ?? 18
+      ),
+      buying: trade[5] as Address,
+      buyingTokenSymbol:
+        assetRegistry[trade[5] as Address]?.token?.symbol ?? 'Unknown',
+      sellingTokenSymbol: sellingToken?.token?.symbol ?? 'Unknown',
+      endAt: trade[1] as number,
+      selling: sellingToken.token.address,
+      startedAt: Number(startBlock.timestamp),
+      worstCasePrice: +formatUnits(
+        trade[0] as bigint,
+        sellingToken?.token?.decimals ?? 18
+      ),
+      startBlock: Number(startBlock.number),
+      endBlock: Number(trade[2]),
+      isSettled: false,
+      kind: TradeKind.BatchTrade,
+    }
+  } catch (e) {
+    console.error('Failed to get trades', e)
+    return null
+  }
+})
+
 const useDutchTrades = () => {
   const rToken = useRToken()
   const blockNumber = useAtomValue(currentBlockAtom)
   const setOngoingTrades = useSetAtom(ongoingDutchTradesAtom)
   const setPendingTrades = useSetAtom(pendingDutchTradesAtom)
   const [currentEndedTrades, setEndedTrades] = useAtom(endedDutchTradesAtom)
+  const openTrade = useAtomValue(openTradesFromChainAtom)
 
   const { data, error } = useQuery(
     rToken && blockNumber ? ongoingTradesQuery : null,
@@ -120,6 +251,12 @@ const useDutchTrades = () => {
       setOngoingTrades(data.trades)
     }
   }, [JSON.stringify(data)])
+
+  useEffect(() => {
+    if (openTrade) {
+      setOngoingTrades([openTrade])
+    }
+  }, [JSON.stringify(openTrade)])
 
   useEffect(() => {
     // Only update if 1) current obj is empty, or 2) length is updated
